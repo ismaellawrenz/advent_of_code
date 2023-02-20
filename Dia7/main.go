b100ae8ace48db5e3af8a43274459460b4f51243
Programador Syscon <ismaellawrenz@gmail.com>
2023-02-20 14:51:39 -0300

    feito parte 1 do dia 7

--- Dia7/main.go
+++ Dia7/main.go
@@ -10,12 +10,6 @@ import (
 	"strings"
 )
 
-func main() {
-
-	primeiraParte()
-	//segundaParte()
-}
-
 type Pasta struct {
 	Nome     string
 	Arquivos []Arquivo
@@ -30,6 +24,12 @@ type TamanhoPasta struct {
 	Tamanho int
 }
 
+func main() {
+
+	primeiraParte()
+
+}
+
 func primeiraParte() {
 	file, err := os.Open("input.txt")
 	if err != nil {
@@ -42,120 +42,62 @@ func primeiraParte() {
 		log.Fatal(err)
 	}
 
-	//primeiraLinha := true
-	var pastas []Pasta
-
-	pasta := Pasta{Nome: "/"}
-	//pastas = append(pastas, pasta)
+	somaTotal := 0
+	pattern := regexp.MustCompile(`(\d+)`)
 
 	var arquivos []Arquivo
-	//pattern := regexp.MustCompile(`(\d+)`)
-	nivel := ""
-
+	var caminho []string
+	var pastas []Arquivo
 	for scanner.Scan() {
 
-		if strings.Contains(scanner.Text(), "$ cd ") && scanner.Text() != "$ cd .." {
-			dados := strings.Split(scanner.Text(), " ")
-			nivel += "-"
-			fmt.Println(nivel, dados[2], " (dir)")
-		} else if scanner.Text() == "$ cd .." {
-			nivel = nivel[0 : len(nivel)-1]
-		} else if scanner.Text() != "$ ls" {
-			fmt.Println(nivel + scanner.Text())
+		if strings.Contains(scanner.Text(), "$ cd") {
+			if strings.Contains(scanner.Text(), "$ cd ..") {
+				caminho = caminho[0 : len(caminho)-1]
+			} else {
+				dados := strings.Split(scanner.Text(), " ")
+				caminho = append(caminho, dados[2])
+			}
 		}
 
-		/*
-			if !primeiraLinha {
+		match := pattern.FindAllString(scanner.Text(), -1)
+		if len(match) > 0 {
+			dados := strings.Split(scanner.Text(), " ")
+			tamanho, _ := strconv.Atoi(dados[0])
+			arquivo := Arquivo{Nome: strings.Join(caminho, `|`) + `|` + dados[1], Tamanho: tamanho}
+			arquivos = append(arquivos, arquivo)
+		}
 
-				if strings.Contains(scanner.Text(), "cd ") && scanner.Text() != "$ cd .." {
-					dados := strings.Split(scanner.Text(), " ")
-					pasta.Arquivos = append(pasta.Arquivos, arquivos...)
-					arquivos = nil
-					pastas = append(pastas, pasta)
-					pasta = Pasta{Nome: dados[2]}
-				}
-
-				match := pattern.FindAllString(scanner.Text(), -1)
-				if len(match) > 0 {
-					dados := strings.Split(scanner.Text(), " ")
-					tamanho, _ := strconv.Atoi(dados[0])
-					arquivo := Arquivo{Nome: dados[1], Tamanho: tamanho}
-					arquivos = append(arquivos, arquivo)
-				}
-
-			} else {
-				primeiraLinha = false
-			}
-		*/
 	}
 
-	pasta.Arquivos = append(pasta.Arquivos, arquivos...)
-	pastas = append(pastas, pasta)
-	somaTotal := 0
-	somaDiretorio := 0
+	for i := 0; i < len(arquivos); i++ {
+		fmt.Println(arquivos[i].Nome, arquivos[i].Tamanho)
+		d := strings.Split(arquivos[i].Nome, "|")
+		var caminhoInteiro = ""
+		for j := 0; j < len(d)-1; j++ {
+			caminhoInteiro += d[j] + "|"
+			achou := false
+			for q := 0; q < len(pastas); q++ {
+				if pastas[q].Nome == caminhoInteiro {
+					pastas[q].Tamanho += arquivos[i].Tamanho
+					achou = true
+				}
+			}
+			if !achou {
+				pasta := Arquivo{Nome: caminhoInteiro, Tamanho: arquivos[i].Tamanho}
+				pastas = append(pastas, pasta)
+			}
+		}
+
+	}
+
 	for i := 0; i < len(pastas); i++ {
-		for j := 0; j < len(pastas[i].Arquivos); j++ {
-			somaDiretorio += pastas[i].Arquivos[j].Tamanho
+		if pastas[i].Tamanho < 100000 {
+			somaTotal += pastas[i].Tamanho
+			fmt.Println("----------------", pastas[i].Nome, " tem ", pastas[i].Tamanho)
 		}
-		if somaDiretorio <= 100000 {
-			somaTotal = somaDiretorio + somaTotal
-		}
-		somaDiretorio = 0
+
 	}
 
 	fmt.Println("Resultado final da primeira parte->", somaTotal)
-
-}
-
-func segundaParte() {
-	file, err := os.Open("input.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
-	pattern := regexp.MustCompile(`(\d+)`)
-	var valores = make([][]string, 9)
-
-	for scanner.Scan() {
-		if strings.Contains(scanner.Text(), ("move")) {
-			match := pattern.FindAllString(scanner.Text(), -1)
-			quantidade, _ := strconv.Atoi(match[0])
-			origem, _ := strconv.Atoi(match[1])
-			destino, _ := strconv.Atoi(match[2])
-
-			origem -= 1
-			destino -= 1
-
-			contador := quantidade - 1
-			for i := 0; i < quantidade; i++ {
-
-				valores[destino] = append([]string{valores[origem][contador-i]}, valores[destino]...)
-
-			}
-			valores[origem] = valores[origem][quantidade:]
-
-			fmt.Print("")
-
-		} else {
-			if scanner.Text() != "" && scanner.Text()[1:2] != "1" {
-				contador := 1
-				for i := 0; i < len(valores); i++ {
-					if scanner.Text()[contador:contador+1] != " " {
-						valores[i] = append(valores[i], scanner.Text()[contador:contador+1])
-					}
-					contador += 4
-				}
-			}
-		}
-	}
-	fmt.Println("Resultado final da segunda parte->")
-	for i := 0; i < len(valores); i++ {
-		fmt.Print(valores[i][0])
-	}
 
 }
