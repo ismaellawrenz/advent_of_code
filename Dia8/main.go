2d2bb26e76ad41f795a5457834237f5ff4d63bf0
Programador Syscon <ismaellawrenz@gmail.com>
2023-02-22 17:12:21 -0300

    feito dia 8

--- Dia8/main.go
+++ Dia8/main.go
@@ -25,7 +25,7 @@ type TamanhoPasta struct {
 func main() {
 
 	primeiraParte()
-
+	segundaParte()
 }
 
 func primeiraParte() {
@@ -108,5 +108,83 @@ func primeiraParte() {
 	}
 
 	fmt.Println("Resultado final da primeira parte->", visivel)
+
+}
+
+func segundaParte() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	var matriz [][]string
+	for scanner.Scan() {
+		d := strings.Split(scanner.Text(), "")
+		matriz = append(matriz, d)
+	}
+
+	maiorValor := 0
+	for linha := 1; linha < len(matriz)-1; linha++ {
+
+		for coluna := 1; coluna < len(matriz[linha])-1; coluna++ {
+
+			//verifica a esquerda
+			contadorEsquerda := 0
+			for d := coluna - 1; d >= 0; d-- {
+				if matriz[linha][d] < matriz[linha][coluna] {
+					contadorEsquerda += 1
+				} else {
+					contadorEsquerda += 1
+					break
+				}
+			}
+
+			//verifica a direita
+			contadorDireita := 0
+			for d := coluna + 1; d < len(matriz[linha]); d++ {
+				if matriz[linha][d] < matriz[linha][coluna] {
+					contadorDireita += 1
+
+				} else {
+					contadorDireita += 1
+					break
+				}
+			}
+
+			//verifica a coluna superior
+			contadorSuperior := 0
+			for d := linha - 1; d >= 0; d-- {
+				if matriz[d][coluna] < matriz[linha][coluna] {
+					contadorSuperior += 1
+
+				} else {
+					contadorSuperior += 1
+					break
+				}
+			}
+
+			//verifica a coluna inferior
+			contadorInferior := 0
+			for d := linha + 1; d < len(matriz); d++ {
+				if matriz[d][coluna] < matriz[linha][coluna] {
+					contadorInferior += 1
+				} else {
+					contadorInferior += 1
+					break
+				}
+			}
+			resultado := contadorDireita * contadorEsquerda * contadorInferior * contadorSuperior
+			if resultado > maiorValor {
+				maiorValor = resultado
+			}
+		}
+	}
+
+	fmt.Println("Resultado final da segunda parte->", maiorValor)
 
 }
